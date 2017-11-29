2f1d2e9996510670ac0763bf6701df1bc7a00d9b
gunay <gunay@inc053000005.lancs.local>
2017-11-29 17:55:28 +0000

    Create AchievementPropertyTest

---
+++ src/test/java/tr/edu/boun/cmpe/mas/akin/hammurabi/property/AchievementPropertyTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.edu.boun.cmpe.mas.akin.hammurabi.property;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gunay
+ */
+public class AchievementPropertyTest {
+    
+    public AchievementPropertyTest() {
+    }
+    
+}
